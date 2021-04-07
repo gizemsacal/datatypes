90816309c444ef29d6dd9afc31d2d69bb8b05a00
gizemsacal <46420726+gizemsacal@users.noreply.github.com>
2021-04-07 10:35:46 +0300

    data types examples

---
+++ src/com/java/ex/Main.java
@@ -0,0 +1,19 @@
+package com.java.ex;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double sayi = 12.5;
+	sayi= -129;
+	char karakter = 'A';
+	/* char tek karakter tutmak istediğimizde tek tırnak
+	kullanıyoruz. ama A' nın yanına birde B yazarsak kod kızacaktır.
+	 */
+        boolean dogruMu=false;
+        /*boolean true veya false. 1 veya 0 değerlerini tutan bir tiptir.
+
+         */
+
+    }
+}
+
